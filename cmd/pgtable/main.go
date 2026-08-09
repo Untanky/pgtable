72c47d812edd2d0602f62ff291d02a6c465f40bb
Lukas Grimm <lukaskingsmail@gmail.com>
2026-08-09 16:02:15 +0200

    Display version

--- cmd/pgtable/main.go
+++ cmd/pgtable/main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	tea "charm.land/bubbletea/v2"
+	flag "github.com/spf13/pflag"
 	"github.com/untanky/pgtable/internal/parser"
 	"github.com/untanky/pgtable/internal/tui"
 	"github.com/untanky/pgtable/render"
 )
 
 func main() {
+	var (
+		showVersion = flag.BoolP("version", "v", false, "Display the application's version")
+	)
+	flag.Parse()
+
+	if *showVersion {
+		displayVersions()
+	}
+
 	checkStdin()
 
 	table, err := parser.Parse(os.Stdin)
@@ -37,7 +48,7 @@ func checkStdin() {
 	}
 
 	if info.Mode()&os.ModeCharDevice != 0 {
-		fmt.Fprintln(os.Stderr,"Please pipe in data; interactive mode not supported!")
+		fmt.Fprintln(os.Stderr, "Please pipe in data; interactive mode not supported!")
 		os.Exit(1)
 		return
 	}
---
+++ cmd/pgtable/version.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+)
+
+var AppVersion = ""
+
+func displayVersions() {
+	version, goVersion := getVersion()
+
+	fmt.Fprintf(os.Stdin, "Version: %s\nGo Version: %s\n", version, goVersion)
+	os.Exit(0)
+}
+
+func getVersion() (string, string) {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown", "unknown"
+	}
+
+	if AppVersion != "" {
+		return AppVersion, info.GoVersion
+	}
+
+	if info.Main.Version != "" {
+		return info.Main.Version, info.GoVersion
+	}
+
+	return "development", info.GoVersion
+}
